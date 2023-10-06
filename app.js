b49e213fd76d8ff356a1e918a18973dd4ef82cc9
Sackah <81268132+Sackah@users.noreply.github.com>
2023-10-06 15:06:06 +0000

    udpates

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const app = express();
 
 app.listen(4000);
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
     console.log('request made');
-    res.sendFile(`${__dirname}/views/index.html`);
+    res.redirect('/blogs');
 });
+app.get('/blogs', (req, res)=>{
+    res.render('index', {title: 'Home'});
+})
+app.get('/about', (req, res)=>{
+    res.render('about', {title: 'About'});
+})
+app.get('/create', (req, res)=>{
+    res.render('create', {title: 'Create Blog'});
+})
