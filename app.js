ec8b8f777ec85e06b9ec71421709445b1624c59b
Sackah <81268132+Sackah@users.noreply.github.com>
2023-10-06 17:34:01 +0000

    updates

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ app.get('/', (req, res)=>{
     res.redirect('/blogs');
 });
 app.get('/blogs', (req, res)=>{
-    res.render('index', {title: 'Home'});
+    let curDate = new Date;
+    const date = convertToStandardTime(curDate.toISOString()); 
+    const blogs = [
+        {title: 'First blog', snippet: 'this is the first blog', author: 'NY Times', date, id: '1'},
+        {title: 'Second blog', snippet: 'this is the second blog', author: 'yoshi', date, id: '2'},
+        {title: 'Third blog', snippet: 'this is the third blog', author: 'mario', date, id:'3'}
+    ]
+
+    res.render('index', {title: 'Home', blogs});
 })
 app.get('/about', (req, res)=>{
     res.render('about', {title: 'About'});
@@ -20,3 +28,30 @@ app.get('/about', (req, res)=>{
 app.get('/create', (req, res)=>{
     res.render('create', {title: 'Create Blog'});
 })
+
+
+//helper functions
+function convertToStandardTime(isoDateString) {
+    // Create a new Date object from the ISO date string
+    let date = new Date(isoDateString);
+
+    // Define options for toLocaleString method
+    let options = { 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit',
+        hour12: true
+    };
+
+    // Convert date to specified format
+    let standardTime = date.toLocaleString('en-US', options);
+
+    // Replace comma before time with "at"
+    standardTime = standardTime.replace(',', ' at');
+
+    return standardTime;
+}
+
+//console.log(convertToStandardTime('2023-10-05T21:44:06.703+00:00'));
